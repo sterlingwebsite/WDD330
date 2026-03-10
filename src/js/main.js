e2eb7bf059a8edc4b3257da8223b189f5bede358
sterlingwebsite <ssteele1@byupathway.edu>
2026-03-10 16:11:42 -0600

    added lint fix script

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
-import ProductData from './ProductData.mjs';
-import ProductList from './ProductList.mjs';
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
 
 const dataSource = new ProductData("tents");
 
