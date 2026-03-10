2bab24f85e85b1c5a8f336cc6a6e0faab7ab6f0d
sterlingwebsite <ssteele1@byupathway.edu>
2026-03-10 16:11:05 -0600

    W02 Individual Activity: Dynamic Product List

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,6 +1,7 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 export default class ProductDetails {
+    
     constructor(productId, dataSource) {
         this.productId = productId;
         this.product = {};
