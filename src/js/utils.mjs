d32918e045da66d80a113ac745b5332b630f9f92
sterlingwebsite <ssteele1@byupathway.edu>
2026-03-10 14:04:51 -0600

    W02 Team Activity: Dynamic Product Detail Page

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,3 +21,18 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+// get the product id from the query string
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get(param);
+}
+
+export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
+  const htmlStrings = list.map(template);
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
