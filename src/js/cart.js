a60c115e48d701ddc448cf4abd0c7a9399b737be
sterlingwebsite <ssteele1@byupathway.edu>
2026-03-12 12:47:58 -0600

    W02 Individual Task(s): Report Empty Card Error: cart.html fixed

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,8 +1,19 @@
 import { getLocalStorage } from "./utils.mjs";
 
+// function renderCartContents() {
+//   const cartItems = getLocalStorage("so-cart");
+//   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+//   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+// }
+
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  let cartItems = getLocalStorage("so-cart") || [];
+
+  if (!Array.isArray(cartItems)) {
+    cartItems = [cartItems];
+  }
+
+  const htmlItems = cartItems.map(item => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 
