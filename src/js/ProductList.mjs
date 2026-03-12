e3f87365eae5504140245ae5996d2d63de7abf87
bahintingchristopher <bahintingchristopher@gmail.com>
2026-03-13 03:07:47 +0800

    Master (#7)
    * fix: change 'products' to 'product' in template link to fix data loading.
      ( <a href="product_pages/?product=${product.Id}"> ) of the productList.mjs file.
    
    * modefied the reason

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,9 +1,8 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
-  return `
-    <li class="product-card">
-      <a href="product_pages/?products=${product.Id}">
+  return `<li class="product-card">
+      <a href="product_pages/?product=${product.Id}"> 
         <img src="${product.Image}" alt="${product.Name}">
         <h2>${product.Brand.Name}</h2>
         <h3>${product.Name}</h3>
