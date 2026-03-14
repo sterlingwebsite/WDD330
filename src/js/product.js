77d0c8b774208c96d1f24180f746e5dfc74d8ae4
sterlingwebsite <ssteele1@byupathway.edu>
2026-03-14 17:14:06 -0600

    W03 Individual Activity: Expand Inventory

--- src/js/product.js
+++ src/js/product.js
@@ -9,14 +9,3 @@ const productId = getParam("product");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
-
-// add to cart button event handler
-// async function addToCartHandler(e) {
-//   const product = await dataSource.findProductById(e.target.dataset.id);
-//   addProductToCart(product);
-// }
-
-// add listener to Add to Cart button
-// document
-//   .getElementById("addToCart")
-//   .addEventListener("click", addToCartHandler);
