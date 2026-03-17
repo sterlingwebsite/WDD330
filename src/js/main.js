1f2dbc94e2d46415ccb8e3f8400fc82e59503a0f
Christopher Bahinting <bahintingchristopher@gmail.com>
2026-03-17 23:59:40 +0800

    refactor main page from static to dynamic

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,22 @@
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
+
+//the 4 categories in main page
+const categories = ["tents", "backpacks", "sleeping-bags", "hammocks"];
+
+function renderCategories(list) {
+  const container = document.querySelector(".product-list");
+  
+  const mainPageCard = list.map(category =>`
+         <li>
+            <a href="product_listing/index.html?category=${category}">
+                <img src="images/category-${category.replace('-', '')}.svg"  alt="${category}">
+                <h2>${category.charAt(0).toUpperCase() +category.slice(1).replace('-', ' ')}</h2>
+            </a>
+         </li>`);
+
+  container.innerHTML = mainPageCard.join("");
+}
+
+renderCategories(categories);
