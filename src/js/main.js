6a90597be8428faeb4ef436d8f29a92e202423d8
bahintingchristopher <bahintingchristopher@gmail.com>
2026-03-20 05:57:38 +0800

    Add a superscript number of items in the cart to the backpack icon. (#29)
    * modification made in ProductData.mjs to load the  product content of each category
    
    * Add a superscript number of items in the cart to the backpack icon.
    
    * adjust padding
    
    * fix: added async init and cart counter to product listing
    
    * back to original code
    
    ---------
    
    Co-authored-by: sterlingwebsite <131792759+sterlingwebsite@users.noreply.github.com>

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,12 @@
 import { loadHeaderFooter } from "./utils.mjs";
+import { updateCartInventory } from "./cartCounter.mjs";
 
-loadHeaderFooter();
-
-//the 4 categories in main page
-const categories = ["tents", "backpacks", "sleeping-bags", "hammocks"];
-
-function renderCategories(list) {
-  const container = document.querySelector(".product-list");
-  
-  const mainPageCard = list.map(category =>`
-         <li>
-            <a href="product_listing/index.html?category=${category}">
-                <img src="images/category-${category.replace('-', '')}.svg"  alt="${category}">
-                <h2>${category.charAt(0).toUpperCase() +category.slice(1).replace('-', ' ')}</h2>
-            </a>
-         </li>`);
-
-  container.innerHTML = mainPageCard.join("");
+async function init() {
+  //Wait for the header to load into the main page
+  await loadHeaderFooter();
+ 
+  updateCartInventory();
 }
 
-renderCategories(categories);
+//to inialize
+init();
