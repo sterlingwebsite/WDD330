67b27186a028393371f03a3d37864ab14a39b9b5
sterlingwebsite <ssteele1@byupathway.edu>
2026-03-19 16:12:26 -0600

    made icons appear on home page again

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,36 @@
 import { loadHeaderFooter } from "./utils.mjs";
 import { updateCartInventory } from "./cartCounter.mjs";
 
-async function init() {
-  //Wait for the header to load into the main page
-  await loadHeaderFooter();
- 
-  updateCartInventory();
+// the 4 categories in main page
+const categories = ["tents", "backpacks", "sleeping-bags", "hammocks"];
+
+function renderCategories(list) {
+  const container = document.querySelector(".product-list");
+
+  const mainPageCard = list.map(
+    (category) => `
+    <li>
+      <a href="product_listing/index.html?category=${category}">
+        <img src="images/category-${category.replace("-", "")}.svg" alt="${category}">
+        <h2>${category.charAt(0).toUpperCase() + category.slice(1).replace("-", " ")}</h2>
+      </a>
+    </li>
+  `,
+  );
+
+  container.innerHTML = mainPageCard.join("");
 }
 
-//to inialize
+async function init() {
+  // Wait for header/footer to load
+  await loadHeaderFooter();
+
+  // Update cart bubble
+  updateCartInventory();
+
+  // Render homepage categories
+  renderCategories(categories);
+}
+
+// Initialize
 init();
