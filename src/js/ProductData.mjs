6395e22ff9454b2d9ddcf35d4916e3e5d82b70bf
Christopher Bahinting <bahintingchristopher@gmail.com>
2026-03-21 08:27:11 +0800

    Fixing image in the cart page.

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -21,7 +21,7 @@ export default class ProductData {
     return data.Result;
   }
   async findProductById(id) {
-    const response = await fetch(`${baseURL}product/${id}`);
+    const response = await fetch(`${this.baseURL}product/${id}`);
     const data = await convertToJson(response);
     return data.Result;
   }
