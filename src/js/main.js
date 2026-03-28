4b0b88183e42c54bc2325042609bbca6509bdcd9
bahintingchristopher <bahintingchristopher@gmail.com>
2026-03-29 01:26:52 +0800

    implement dynamic alert system from JSON for W04 task (#38)

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import { loadHeaderFooter } from "./utils.mjs";
 import { updateCartInventory } from "./cartCounter.mjs";
+import Alert from "./Alert.mjs";
 
 // the 4 categories in main page
 const categories = ["tents", "backpacks", "sleeping-bags", "hammocks"];
@@ -24,6 +25,16 @@ function renderCategories(list) {
 async function init() {
   // Wait for header/footer to load
   await loadHeaderFooter();
+
+  // for the alert - W04
+  try {
+    const alertManager = new Alert();
+    await alertManager.init(); 
+    console.log("Alerts loaded successfully");
+  } catch (err) {
+    // If alerts fail, the code "catches" the error here and keeps going
+    console.error("Alerts failed to load, but continuing to categories:", err);
+  }
 
   // Update cart bubble
   updateCartInventory();
--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -2,6 +2,7 @@ import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ExternalServices from "./ExternalServices.mjs";
 import ProductList from "./ProductList.mjs";
 import { updateCartInventory } from "./cartCounter.mjs";
+import Alert from "./Alert.mjs";
 
 async function init() {
   await loadHeaderFooter();
@@ -12,6 +13,10 @@ async function init() {
   const dataSource = new ExternalServices();
   const element = document.querySelector(".product-list");
   const productList = new ProductList(category, dataSource, element);
+
+  // alert - W04 individual task
+  const alertHandler = new Alert();
+  alertHandler.init();
 
   productList.init();
 }
